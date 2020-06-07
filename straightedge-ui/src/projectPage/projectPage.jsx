7a2be2a76dd117ea99ab38c28ab61e7443384323
Keith Franklin <keith30895@gmail.com>
2020-06-07 19:38:32 +0530

    addded timeline

--- straightedge-ui/src/projectPage/projectPage.jsx
+++ straightedge-ui/src/projectPage/projectPage.jsx
@@ -40,8 +40,49 @@ export class ProjectPage extends React.Component {
                 </div>
                 <div className='row'>
                     {this.scenarioPicker()}
-                    <div className="projects">
-                    <div className='diamond'></div>
+                    <div className="projects" >
+                        <div className='diamond'></div>
+                        <div style={{ overflow: 'scroll', height: "100%" }}>
+                            <ul className="timeline">
+                                <li>
+                                    <div className="direction-r">
+                                        <div className="flag-wrapper">
+                                            <span className="flag start">click</span>
+                                            <span className="time-wrapper"><span className="time">2013</span></span>
+                                        </div>
+                                        <div className="desc">
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit fugiat suscipit ullam,
+                                            ipsum ut nulla, eligendi est dolor dolores ad natus officiis ducimus quis illum! Soluta quos optio eos.
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit fugiat suscipit ullam,
+                                            ipsum ut nulla, eligendi est dolor dolores ad natus officiis ducimus quis illum! Soluta quos optio eos.
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit fugiat suscipit ullam,
+                                            ipsum ut nulla, eligendi est dolor dolores ad natus officiis ducimus quis illum! Soluta quos optio eos.
+                                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit fugiat suscipit ullam,
+                                            ipsum ut nulla, eligendi est dolor dolores ad natus officiis ducimus quis illum! Soluta quos optio eos.
+                                        <br />
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugit fugiat suscipit ullam,
+                                        ipsum ut nulla, eligendi est dolor dolores ad natus officiis ducimus quis illum! Soluta quos optio eos.
+                                    </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="direction-r">
+                                        <div className="flag-wrapper">
+                                            <span className="flag">click</span>
+                                            <span className="time-wrapper"><span className="time">2013</span></span>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="direction-l">
+                                        <div className="flag-wrapper">
+                                            <span className="flag">click</span>
+                                            <span className="time-wrapper"><span className="time">2013</span></span>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
